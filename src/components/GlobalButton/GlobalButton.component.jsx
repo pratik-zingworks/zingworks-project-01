bfd6c2c5c77816fa45e73e1925b0ce646d01e6d8
pratik sonar <pratik.sonar@zingworks.in>
2021-10-04 17:01:09 +0530

    changes made in button hover bgcolor

--- src/components/GlobalButton/GlobalButton.component.jsx
+++ src/components/GlobalButton/GlobalButton.component.jsx
@@ -5,7 +5,10 @@ const GlobalButton = ({Title, Type}) => {
     return ( 
         <div className="Global-button">
             <Button variant="contained" type={Type} sx={{
-                backgroundColor: "#6C63FF"
+                backgroundColor: "#6C63FF",
+                "&:hover" : {
+                    backgroundColor: "#37328a",
+                },
             }}>{Title.toUpperCase()}</Button>
         </div>
      );
